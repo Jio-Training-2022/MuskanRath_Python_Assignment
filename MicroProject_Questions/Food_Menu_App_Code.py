9fd2ef4117f3ad6f7e2658a16be7202b89a5e84c
mr3012 <113744720+mr3012@users.noreply.github.com>
2022-10-23 23:15:48 +0530

    Delete Food_Menu_App_Code.py

--- MicroProject_Questions/Food_Menu_App_Code.py
+++
@@ -1,9 +0,0 @@
-# List of items on the menu stored in the form of list
-items = ['Biriyani','Pulao','Palak Paneer','Dal Makhani','Butter Chicken','Butter Naan','Hot and Sour Soup','Paneer Roll','Jeera Rice'] 
-# User gives in input an item amongst the list of items stored on the menu
-i = input('Enter the item you want to choose: ')
-# if the input item, i is present in the items list, items; then the item will be printed. Else the user will be asked to enter a valid item name
-if i in items:
-  print(i)   # printing the item
-else:
-  print("Please enter a valid item name.") # if item does not exist in list of items then user has to enter valid item
